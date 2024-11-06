88ee895f12b45aa499be332ea24b93427ca44175
Axel Londono <axxl1306@gmail.com>
2024-11-06 13:21:02 -0500

    commit entities dto

--- src/main/java/com/estramipyme/crud/dto/request/CreateQuestionDTO.java
+++ src/main/java/com/estramipyme/crud/dto/request/CreateQuestionDTO.java
@@ -1,4 +1,33 @@
 package com.estramipyme.crud.dto.request;
 
+import java.util.List;
+
 public class CreateQuestionDTO {
+    private String title; // Título de la pregunta
+    private List<CreateOptionDTO> options; // Lista de opciones
+
+    public CreateQuestionDTO() {}
+
+    public CreateQuestionDTO(String title, List<CreateOptionDTO> options) {
+        this.title = title;
+        this.options = options;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<CreateOptionDTO> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<CreateOptionDTO> options) {
+        this.options = options;
+    }
 }
+
+
--- src/main/java/com/estramipyme/crud/dto/request/CreateResponseDTO.java
+++ src/main/java/com/estramipyme/crud/dto/request/CreateResponseDTO.java
@@ -1,4 +1,39 @@
 package com.estramipyme.crud.dto.request;
 
 public class CreateResponseDTO {
+    private Long userId;      // ID del usuario que responde
+    private Long questionId;   // ID de la pregunta
+    private Long selectedOptionId; // ID de la opción seleccionada
+
+    public CreateResponseDTO() {}
+
+    public CreateResponseDTO(Long userId, Long questionId, Long selectedOptionId) {
+        this.userId = userId;
+        this.questionId = questionId;
+        this.selectedOptionId = selectedOptionId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Long questionId) {
+        this.questionId = questionId;
+    }
+
+    public Long getSelectedOptionId() {
+        return selectedOptionId;
+    }
+
+    public void setSelectedOptionId(Long selectedOptionId) {
+        this.selectedOptionId = selectedOptionId;
+    }
 }
