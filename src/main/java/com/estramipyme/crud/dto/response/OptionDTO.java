69fe79812be40dcd20965cb6db40dc96546bf583
mateosorion <mateo_osorion@soy.sena.edu.co>
2024-11-08 07:20:12 -0500

    OptionDTO Actualizado

--- src/main/java/com/estramipyme/crud/dto/response/OptionDTO.java
+++ src/main/java/com/estramipyme/crud/dto/response/OptionDTO.java
@@ -1,4 +1,40 @@
 package com.estramipyme.crud.dto.response;
 
 public class OptionDTO {
+
+    private Long id;
+    private String questionId;
+    private String optionText;
+
+    public OptionDTO() {}
+
+    public OptionDTO(Long id, String questionId, String optionText) {
+        this.id = id;
+        this.questionId = questionId;
+        this.optionText = optionText;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(String questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getOptionText() {
+        return optionText;
+    }
+
+    public void setOptionText(String optionText) {
+        this.optionText = optionText;
+    }
 }
