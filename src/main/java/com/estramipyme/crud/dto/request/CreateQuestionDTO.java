224573fea7a9d9edf39f2dba14a397218335f32f
mateosorion <mateo_osorion@soy.sena.edu.co>
2024-11-14 03:58:35 -0500

    Update CreateQuestionDTO

--- src/main/java/com/estramipyme/crud/dto/request/CreateQuestionDTO.java
+++ src/main/java/com/estramipyme/crud/dto/request/CreateQuestionDTO.java
@@ -1,33 +1,11 @@
 package com.estramipyme.crud.dto.request;
 
-import java.util.List;
+import lombok.Data;
 
+@Data
 public class CreateQuestionDTO {
-    private String title; // Título de la pregunta
-    private List<CreateOptionDTO> options; // Lista de opciones
-
-    public CreateQuestionDTO() {}
-
-    public CreateQuestionDTO(String title, List<CreateOptionDTO> options) {
-        this.title = title;
-        this.options = options;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public List<CreateOptionDTO> getOptions() {
-        return options;
-    }
-
-    public void setOptions(List<CreateOptionDTO> options) {
-        this.options = options;
-    }
+    private String section;
+    private String statement;
+    private String subsection;
 }
-
 
