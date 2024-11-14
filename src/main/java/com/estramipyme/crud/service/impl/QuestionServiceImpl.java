7db3b33716dd7769dddec69f73f876350d9d31bc
mateosorion <mateo_osorion@soy.sena.edu.co>
2024-11-14 04:01:06 -0500

    Update QuestionServiceImpl

--- src/main/java/com/estramipyme/crud/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/estramipyme/crud/service/impl/QuestionServiceImpl.java
@@ -1,4 +1,55 @@
 package com.estramipyme.crud.service.impl;
 
-public class QuestionServiceImpl {
+import com.estramipyme.crud.dto.request.CreateQuestionDTO;
+import com.estramipyme.crud.dto.response.QuestionDTO;
+import com.estramipyme.crud.persistence.entities.Question;
+import com.estramipyme.crud.persistence.repositories.QuestionRepository;
+import com.estramipyme.crud.service.interfaces.IQuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class QuestionServiceImpl implements IQuestionService {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Override
+    public QuestionDTO createQuestion(CreateQuestionDTO createQuestionDTO) {
+        Question question = new Question();
+        question.setSection(createQuestionDTO.getSection());
+        question.setStatement(createQuestionDTO.getStatement());
+        question.setSubsection(createQuestionDTO.getSubsection());
+        question = questionRepository.save(question);
+        return new QuestionDTO(question);
+    }
+
+    @Override
+    public QuestionDTO getQuestionById(Long id) {
+        Question question = questionRepository.findById(id).orElseThrow(() -> new RuntimeException("Question not found"));
+        return new QuestionDTO(question);
+    }
+
+    @Override
+    public List<QuestionDTO> getAllQuestions() {
+        return questionRepository.findAll().stream().map(QuestionDTO::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public QuestionDTO updateQuestion(Long id, CreateQuestionDTO createQuestionDTO) {
+        Question question = questionRepository.findById(id).orElseThrow(() -> new RuntimeException("Question not found"));
+        question.setSection(createQuestionDTO.getSection());
+        question.setStatement(createQuestionDTO.getStatement());
+        question.setSubsection(createQuestionDTO.getSubsection());
+        question = questionRepository.save(question);
+        return new QuestionDTO(question);
+    }
+
+    @Override
+    public void deleteQuestion(Long id) {
+        questionRepository.deleteById(id);
+    }
 }
