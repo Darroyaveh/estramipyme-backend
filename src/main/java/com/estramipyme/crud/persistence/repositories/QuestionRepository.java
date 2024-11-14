1680e45215b6ebfaf6e476ebd016721e97de97cf
mateosorion <mateo_osorion@soy.sena.edu.co>
2024-11-14 04:00:47 -0500

    Update QuestionRepository

--- src/main/java/com/estramipyme/crud/persistence/repositories/QuestionRepository.java
+++ src/main/java/com/estramipyme/crud/persistence/repositories/QuestionRepository.java
@@ -1,4 +1,9 @@
 package com.estramipyme.crud.persistence.repositories;
 
-public class QuestionRepository {
+import com.estramipyme.crud.persistence.entities.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
